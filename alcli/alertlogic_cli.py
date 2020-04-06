8e6062942308def646d3dc0cc5231930ce3423ca
Pavel Trakhtman <pavel@alertlogic.com>
2020-04-06 16:57:08 -0500

    Support session initialization from parsed globals. Fixed lint errors

--- alcli/alertlogic_cli.py
+++ alcli/alertlogic_cli.py
@@ -8,13 +8,10 @@ import json
 import jmespath
 from json import JSONDecodeError
 import logging
-import argparse
 import pydoc
 
 from urllib.parse import urlparse
 from collections import OrderedDict
-from pydoc import pager
-
 
 import almdrlib
 from almdrlib.session import Session
@@ -25,7 +22,6 @@ from almdrlib.region import Residency
 from almdrlib import __version__ as almdrlib_version
 
 from alcli.cliparser import ALCliArgsParser
-from alcli.cliparser import USAGE
 from alcli.clihelp import ALCliMainHelpFormatter
 from alcli.clihelp import ALCliServiceHelpFormatter
 from alcli.clihelp import ALCliOperationHelpFormatter
@@ -37,8 +33,6 @@ logger = logging.getLogger('alcli.almdr_cli')
 LOG_FORMAT = (
     '%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s')
 
-#almdrlib.set_logger('almdrlib.session', logging.DEBUG, format_string=LOG_FORMAT)
-
 GLOBAL_ARGUMENTS = [
         'access_key_id',
         'secret_key',
@@ -48,6 +42,7 @@ GLOBAL_ARGUMENTS = [
         'query',
         'debug'
     ]
+
 
 class AlertLogicCLI(object):
     def __init__(self, session=None):
@@ -62,17 +57,17 @@ class AlertLogicCLI(object):
         parser = self._create_parser(services)
 
         parsed_args, remaining = parser.parse_known_args(args)
-        logger.debug(f"Parsed Arguments: {parsed_args}, Remaining: {remaining}")
-        
+        logger.debug(
+                f"Parsed Arguments: {parsed_args}, Remaining: {remaining}")
+
         if parsed_args.service == 'help' or parsed_args.service is None:
             help_formatter = ALCliMainHelpFormatter(services.keys())
             self._show_help(help_formatter)
             return 128
-            #sys.stderr.write(f"usage: {USAGE}\n")
-            #return 128
 
         if parsed_args.operation == 'help':
-            help_formatter = ALCliServiceHelpFormatter(self._services[parsed_args.service])
+            help_formatter = ALCliServiceHelpFormatter(
+                        self._services[parsed_args.service])
             self._show_help(help_formatter)
             return 128
 
@@ -80,13 +75,12 @@ class AlertLogicCLI(object):
                 hasattr(parsed_args, 'service') and \
                 hasattr(parsed_args, 'operation') and \
                 parsed_args.help == 'help':
-            operation = self._services[parsed_args.service].operations.get(parsed_args.operation)
+            operation = self._services[parsed_args.service].operations.get(
+                                                        parsed_args.operation)
             help_page = ALCliOperationHelpFormatter(operation)
             pydoc.pipepager(help_page.format_page() + '\n', 'less -R')
             return 0
 
-
-        return services[parsed_args.service](remaining, parsed_args)
         try:
             return services[parsed_args.service](remaining, parsed_args)
         except Exception as e:
@@ -98,32 +92,46 @@ class AlertLogicCLI(object):
             self._services = OrderedDict()
             services_list = Session.list_services()
             self._services = {
-                service_name: ServiceOperation(name=service_name, session=self._session)
+                service_name: ServiceOperation(name=service_name,
+                                               session=self._session)
                 for service_name in services_list
             }
 
         return self._services
 
     def _create_parser(self, services):
+        version = "alcli/{} Python/{} almdrlib/{}".format(
+                alcli_version, platform.python_version(), almdrlib_version)
+
         parser = ALCliArgsParser(
                 services,
-                f"alcli/{alcli_version} Python/{platform.python_version()} almdrlib/{almdrlib_version}",
+                version,
                 "Alert Logic CLI Utility",
                 prog="alcli")
-        
+
         # Add Global Options
-        parser.add_argument('--access_key_id', dest='access_key_id', default=None)
-        parser.add_argument('--secret_key', dest='secret_key', default=None)
-        parser.add_argument('--profile', dest='profile', default=None)
-        parser.add_argument('--residency', dest='residency', default=None, choices=Residency.list_residencies())
-        parser.add_argument('--endpoint', dest='global_endpoint', default=None, choices=Region.list_endpoints())
-        parser.add_argument('--query', dest='query', default=None)
-        parser.add_argument('--debug', dest='debug', default=False, action="store_true")
+        parser.add_argument('--access_key_id',
+                            dest='access_key_id', default=None)
+        parser.add_argument('--secret_key',
+                            dest='secret_key', default=None)
+        parser.add_argument('--profile',
+                            dest='profile',  default=None)
+        parser.add_argument('--residency',
+                            dest='residency', default=None,
+                            choices=Residency.list_residencies())
+        parser.add_argument('--endpoint',
+                            dest='global_endpoint', default=None,
+                            choices=Region.list_endpoints())
+        parser.add_argument('--query',
+                            dest='query', default=None)
+        parser.add_argument('--debug',
+                            dest='debug', default=False, action="store_true")
         return parser
 
     def _show_help(self, help_formatter):
         pydoc.pipepager(help_formatter.format_page() + '\n', 'less -R')
         return 0
+
 
 class ServiceOperation(object):
     """
@@ -133,8 +141,7 @@ class ServiceOperation(object):
     def __init__(self, name, session):
         self._name = name
         self._session = session
-        self._client= None
-        #self._service = None
+        self._client = None
         self._description = None
         self._operations = None
 
@@ -144,21 +151,26 @@ class ServiceOperation(object):
         for name, value in parsed_globals.__dict__.items():
             if name == 'operation':
                 operation_name = value
-            elif name == 'debug': 
+            elif name == 'debug':
                 if value:
-                    almdrlib.set_logger('almdrlib', logging.DEBUG, format_string=LOG_FORMAT)
+                    almdrlib.set_logger(
+                        'almdrlib', logging.DEBUG, format_string=LOG_FORMAT)
             elif name == 'service':
                 continue
             elif name in GLOBAL_ARGUMENTS:
                 continue
             else:
                 kwargs[name] = value
-        
+
         service = self._init_service(parsed_globals)
         operation = service.operations.get(operation_name, None)
         if operation:
             # Remove optional arguments that haven't been supplied
-            op_args = {k:self._encode(operation, k, v) for (k,v) in kwargs.items() if v is not None}
+            op_args = {
+                        k: self._encode(operation, k, v)
+                        for (k, v) in kwargs.items() if v is not None
+                    }
+
             res = operation(**op_args)
             self._print_result(res.json(), parsed_globals.query)
 
@@ -189,15 +201,20 @@ class ServiceOperation(object):
         return self._operations
 
     def _init_service(self, parsed_globals):
-        return self._session.client(
-                self._name,
-                profile=parsed_globals.profile)
+        s = Session(
+                access_key_id=parsed_globals.access_key_id,
+                secret_key=parsed_globals.secret_key,
+                profile=parsed_globals.profile,
+                global_endpoint=parsed_globals.global_endpoint,
+                residency=parsed_globals.residency
+                )
+        return s.client(self._name)
 
     def _encode(self, operation, param_name, param_value):
         p = urlparse(param_value)
         if p.scheme == "file":
             value_file_path = os.path.abspath(os.path.join(p.netloc, p.path))
-            with open (value_file_path, "r") as value_file:
+            with open(value_file_path, "r") as value_file:
                 param_value = value_file.read()
 
         schema = operation.get_schema()
@@ -206,11 +223,11 @@ class ServiceOperation(object):
         type = parameter[OpenAPIKeyWord.TYPE]
         if type in OpenAPIKeyWord.SIMPLE_DATA_TYPES:
             return param_value
-        
+
         if type == OpenAPIKeyWord.OBJECT:
             try:
                 return json.loads(param_value)
-            except JSONDecodeError as e:
+            except JSONDecodeError:
                 #
                 # this could be an argument encoded using a short form such as
                 # key1=value,key2=value2
@@ -218,16 +235,17 @@ class ServiceOperation(object):
                 result = dict(
                         (k.strip(), v.strip())
                         for k, v in (kv.split('=')
-                        for kv in param_value.split(','))
+                                     for kv in param_value.split(','))
                     )
                 return result
-        
-        # TODO Raise an exception if we can't build a dictionary from the provided input
+
+        # TODO: Raise an exception if we can't build a dictionary
+        # from the provided input
         return param_value
 
     def _print_result(self, result, query):
         if query:
-            result = jmespath.search(query, result) 
+            result = jmespath.search(query, result)
         print(f"{json.dumps(result, sort_keys=True, indent=4)}")
 
 
@@ -239,9 +257,9 @@ def main():
         logging.basicConfig(level=logging.WARNING)
 
     session = Session()
-    cli= AlertLogicCLI(session=session)
+    cli = AlertLogicCLI(session=session)
     cli.main()
+
 
 if __name__ == "__main__":
     main()
-
