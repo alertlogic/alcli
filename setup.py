8fcbd375a2dd64eb490474c5bf27c5e22951aa66
Anton B <anton.benkevich@gmail.com>
2020-08-07 16:24:04 +0100

    bump sdk version to v1.0.39 (#35)
    bump sdk version to v1.0.39

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ try:
     sdk_latest_version = defs_info['info']['version']
     sdk_dependency = 'alertlogic-sdk-python>=' + sdk_latest_version
 except:
-    sdk_dependency = 'alertlogic-sdk-python>=1.0.38'
+    sdk_dependency = 'alertlogic-sdk-python>=1.0.39'
 
 requirements = [
         sdk_dependency,
