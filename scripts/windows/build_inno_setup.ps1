878e165939a68f7a080225f128be99a49026b727
Anton Benkevich <abenkevich@alertlogic.com>
2021-01-09 07:38:38 +0000

    Use different timestamp sign server

--- scripts/windows/build_inno_setup.ps1
+++ scripts/windows/build_inno_setup.ps1
@@ -5,6 +5,6 @@ New-Item -Path "HKCU:\Software\Jordan Russell" -Name "Inno Setup";
 New-Item -Path "HKCU:\Software\Jordan Russell\Inno Setup" -Name "SignTools";
 $registryPath = "HKCU:\Software\Jordan Russell\Inno Setup\SignTools";
 $name = "SignTool0";
-$value = "signtool=C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll /sm /s My /n `"Alert Logic`" `$f";
+$value = "signtool=C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe sign /a /t http://timestamp.globalsign.com/scripts/timstamp.dll  /sm /s My /n `"Alert Logic`" `$f";
 New-ItemProperty -Path $registryPath -Name $name -Value $value;
 c:\innosetup\iscc.exe /dMyAppVersion=$version /Odist scripts/windows/alcli_windows_install.iss
