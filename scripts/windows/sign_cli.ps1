dd6d89cb736c8d11a3ecc2cd5def2e13efc04982
Anton Benkevich <abenkevich@alertlogic.com>
2021-04-20 12:56:16 +0100

    Use different approach for signing

--- scripts/windows/sign_cli.ps1
+++ scripts/windows/sign_cli.ps1
@@ -1,5 +1,4 @@
 $ErrorActionPreference = "Stop"
 Get-Location
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a /s My /debug /n "Alert Logic" /v build\exe.win-amd64-3.8\alcli.exe
-#& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' sign /a /t http://timestamp.digicert.com /sm /s My /debug /n "Alert Logic" /v build\exe.win-amd64-3.8\alcli.exe
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' verify /pa build\exe.win-amd64-3.8\alcli.exe
+$cert=Get-ChildItem -Path Cert:\LocalMachine\My -CodeSigningCert
+Set-AuthenticodeSignature -FilePath build\exe.win-amd64-3.8\alcli.exe -Certificate $cert -IncludeChain All -TimestampServer "http://timestamp.fabrikam.com/scripts/timstamper.dll"
--- scripts/windows/sign_cli_msi.ps1
+++ scripts/windows/sign_cli_msi.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 Get-Location
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' sign /a /t http://timestamp.globalsign.com/scripts/timestamp.dll /sm /s My /debug /n "Alert Logic" /v dist\alcli_setup.msi
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' verify /pa dist\alcli_setup.msi
+$cert=Get-ChildItem -Path Cert:\LocalMachine\My -CodeSigningCert
+Set-AuthenticodeSignature -FilePath dist\alcli_setup.msi -Certificate $cert -IncludeChain All -TimestampServer "http://timestamp.fabrikam.com/scripts/timstamper.dll"
