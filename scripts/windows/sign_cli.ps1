9694b104666aaf11eb064f909f0c84996fb677d2
Anton B <anton.benkevich@gmail.com>
2021-04-21 17:52:14 +0100

    Restore windows build (#50)
    Sign without timestamp server

--- scripts/windows/sign_cli.ps1
+++ scripts/windows/sign_cli.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 Get-Location
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' sign /a /t http://timestamp.globalsign.com/scripts/timstamp.dll /sm /s My /debug /n "Alert Logic" /v build\exe.win-amd64-3.8\alcli.exe
-& 'C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe' verify /pa build\exe.win-amd64-3.8\alcli.exe
+$cert=Get-ChildItem -Path Cert:\LocalMachine\My -CodeSigningCert
+Set-AuthenticodeSignature -FilePath build\exe.win-amd64-3.8\alcli.exe -Certificate $cert -IncludeChain All
